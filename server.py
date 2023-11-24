2696e14648832009027152080d7ffde15e4f2075
mig8at <mig8at@gmail.com>
2023-11-24 17:22:21 -0500

    ok

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -6,6 +8,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'Hola, este es un servidor basico en Python!')
-        
+
 httpd = HTTPServer(('0.0.0.0', int(os.environ.get('PORT', 8080))), SimpleHTTPRequestHandler)
 httpd.serve_forever()
